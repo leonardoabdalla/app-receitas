f1f265090da9227467c94d1c191ad5020691f355
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-03-30 14:34:54 -0300

    ip: requisito até 73

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import FooterComponent from '../components/FooterComponent';
 
-function ExploreDrinks() {
+function ExploreDrinks({ history }) {
+  const handleClickByIngredient = () => {
+    history.push('/explore/drinks/ingredients');
+  };
   return (
-    <FooterComponent />
+    <div>
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => handleClickByIngredient() }
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Surprise me!
+      </button>
+      <FooterComponent />
+    </div>
   );
 }
 
-export default ExploreDrinks;
+ExploreDrinks.propTypes = {
+  history: PropTypes.node.isRequired,
+};
+
+export default withRouter(ExploreDrinks);
--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import FooterComponent from '../components/FooterComponent';
 
-function ExploreFoods() {
+function ExploreFoods({ history }) {
+  const handleClickByIngredient = () => {
+    history.push('/explore/foods/ingredients');
+  };
+
+  const handleClickByNationality = () => {
+    history.push('/explore/foods/Nationalities');
+  };
+
   return (
-    <FooterComponent />
+    <div>
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => handleClickByIngredient() }
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-nationality"
+        onClick={ () => handleClickByNationality() }
+      >
+        By Nationality
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Surprise me!
+      </button>
+      <FooterComponent />
+    </div>
   );
 }
 
-export default ExploreFoods;
+ExploreFoods.propTypes = {
+  history: PropTypes.node.isRequired,
+};
+
+export default withRouter(ExploreFoods);
