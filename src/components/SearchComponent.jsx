a4bbf6ff00b39c1285235684b7720026041f782f
Larissa Cristina <larissacristina.nunes@outlook.com>
2022-03-30 14:39:35 -0300

    imp: Add radio buttons

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import MyContext from '../context/MyContext';
 
-const SearchComponent = () => (
-  <div>
-    <input
-      placeholder="Search..."
-      type="text"
-      id="searchInput"
-      name="searchInput"
-      data-testid="search-input"
-    />
-  </div>
-);
+const SearchComponent = () => {
+  const {
+    search,
+    inputValue,
+    setInputValue,
+    setFilter,
+    handleClick,
+  } = useContext(MyContext);
+
+  return (
+    <div>
+      <input
+        placeholder="Search..."
+        type="text"
+        id="searchInput"
+        name="searchInput"
+        value={ inputValue }
+        data-testid="search-input"
+        onChange={ ({ target }) => setInputValue(target.value) }
+      />
+      <label htmlFor="ingredient">
+        <input
+          type="radio"
+          data-testid="ingredient-search-radio"
+          id="ingredient"
+          name="searchType"
+          value="ingredient"
+          onChange={ ({ target }) => setFilter({ [target.name]: target.value }) }
+        />
+        Ingredient
+      </label>
+      <label htmlFor="name">
+        <input
+          type="radio"
+          data-testid="name-search-radio"
+          id="name"
+          name="searchType"
+          value="name"
+          onChange={ ({ target }) => setFilter({ [target.name]: target.value }) }
+        />
+        Name
+      </label>
+      <label htmlFor="letter">
+        <input
+          type="radio"
+          data-testid="first-letter-search-radio"
+          id="letter"
+          name="searchType"
+          value="letter"
+          onChange={ ({ target }) => setFilter({ [target.name]: target.value }) }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="exec-search-btn"
+        onClick={ () => handleClick(search) }
+      >
+        Filter
+      </button>
+    </div>
+  );
+};
 
 export default SearchComponent;
--- src/context/MyProvider.jsx
+++ src/context/MyProvider.jsx
@@ -4,10 +4,31 @@ import MyContext from './MyContext';
 
 const MyProvider = ({ children }) => {
   const [email, setEmail] = useState('');
+  const [filter, setFilter] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const [search, setSearch] = useState({
+    name: inputValue,
+    radio: filter,
+  });
+
+  const handleClick = () => {
+    setSearch({
+      name: inputValue,
+      radio: filter,
+    });
+    console.log(search);
+  };
 
   const contextValue = {
     email,
     updateEmail: (value) => setEmail(value),
+    search,
+    setSearch,
+    inputValue,
+    setInputValue,
+    filter,
+    setFilter,
+    handleClick,
   };
 
   return (
