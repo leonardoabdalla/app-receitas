f9d73b0e05eac47924a35f843c011004ef990013
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-03-29 21:13:30 -0300

    ip: requisito 67

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import FooterComponent from '../components/FooterComponent';
 
-function Explore() {
+function Explore({ history }) {
+  const handleClickExploreFoods = () => {
+    history.push('/explore/foods');
+  };
+
+  const handleClickExploreDrinks = () => {
+    history.push('/explore/drinks');
+  };
   return (
-    <FooterComponent />
+    <div>
+      <button
+        type="button"
+        data-testid="explore-foods"
+        onClick={ () => handleClickExploreFoods() }
+      >
+        Explore Foods
+      </button>
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ () => handleClickExploreDrinks() }
+      >
+        Explore Drinks
+      </button>
+      <FooterComponent />
+    </div>
   );
 }
 
-export default Explore;
+Explore.propTypes = {
+  history: PropTypes.node.isRequired,
+};
+
+export default withRouter(Explore);
