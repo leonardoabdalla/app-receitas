b952144d3f86df223fefcf0e363f15f164b7907e
Vanessa <jamarino.vanessa@hotmail.com>
2022-04-05 18:35:40 -0300

    Imp: Teste do Footer ok e do Profile com 94,44%

---
+++ src/tests/Profile.test.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import Profile from '../pages/Profile';
+import Header from '../components/Header';
+
+describe('Teste a pagina <Profile />', () => {
+  it('Verifica se existe o elemento Profile', () => {
+    renderWithRouter(<Profile />);
+    const profile = screen.getByTestId('profile-email');
+    expect(profile).toBeInTheDocument();
+  });
+
+  it('Verifica se a pagina <Profile /> possui os elementos corretos', () => {
+    renderWithRouter(<Profile />);
+
+    const profileEmail = screen.getByTestId('profile-email');
+    const profileprofileDoneBtn = screen.getByTestId('profile-done-btn');
+    const profileFavoriteBtn = screen.getByTestId('profile-favorite-btn');
+    const profilelogoutBtn = screen.getByTestId('profile-logout-btn');
+
+    expect(profileEmail).toBeInTheDocument();
+    expect(profileprofileDoneBtn).toBeInTheDocument();
+    expect(profileFavoriteBtn).toBeInTheDocument();
+    expect(profilelogoutBtn).toBeInTheDocument();
+  });
+
+  it('Verificando se existe 3 botões', () => {
+    renderWithRouter(<Profile />);
+    const seven = 7;
+    const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(seven);
+  });
+
+  it(`Verifica se quando clica no botão de Done Recipies
+   redireciona para a página certa`, () => {
+    const { history } = renderWithRouter(<Profile />);
+    const profileDoneBtn = screen.getByTestId('profile-done-btn');
+    userEvent.click(profileDoneBtn);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/done-recipes');
+  });
+
+  it(`Verifica se quando clica no botão de Favorite Recipes
+   redireciona para a página certa`, () => {
+    const { history } = renderWithRouter(<Profile />);
+    const profilefavoriteBtn = screen.getByTestId('profile-favorite-btn');
+    userEvent.click(profilefavoriteBtn);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorite-recipes');
+  });
+
+  it(`Verifica se quando clica no botão de Logout
+   redireciona para a página certa`, () => {
+    const { history } = renderWithRouter(<Profile />);
+    const profilelogoutBtn = screen.getByTestId('profile-logout-btn');
+    userEvent.click(profilelogoutBtn);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Teste xxxxxxxxxxxx', () => {
+    renderWithRouter(<Profile />);
+    screen.findByText('email');
+  });
+});
+
+// 25 - Limpar local storage
+
+// npm run test-coverage
